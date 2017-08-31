1044d2a3b42607feba9e22f58e00d6866ac95f73
Sushil L <sushil_samson@hotmail.com>
2017-08-31 10:25:16 +1000

    react router setup

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+const HomePage = () => (
+  <div>
+    <h1>This is home page</h1>
+    <Link to='/login'>
+      Login
+    </Link>
+  </div>
+)
+
+export default HomePage
